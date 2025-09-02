702fe7d32722bcd71c9101b9349fe4597554a738
JRReduta <jrreduta@yahoo.com>
2025-09-02 01:50:12 -0700

    Finished schema + seeding

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,34 @@
 import db from "#db/client";
-
-import { createTask } from "#db/queries/tasks";
-import { createUser } from "#db/queries/users";
+import bcrypt from "bcrypt";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seed() {
-  // TODO
+async function seed(numUsers = 3, numTasks = 3, saltRounds = 10) {
+  for (let i = 1; i < numUsers + 1; i++) {
+    const username = `username ${i}`;
+    const hash = await bcrypt.hash(`password ${i}`, saltRounds);
+    await db.query({
+      text: `
+                    INSERT INTO users (username, password)
+                    VALUES($1, $2)
+                    RETURNING *
+        `,
+      values: [username, hash],
+    });
+  }
+  for (let i = 1; i < numUsers + 1; i++) {
+    for (let j = 1; j < numTasks + 1; j++) {
+      await db.query({
+        text: `
+                    INSERT INTO tasks (title, user_id)
+                    VALUES ($1, $2)
+                    RETURNING *
+                `,
+        values: [`task ${i}-${j}`, i],
+      });
+    }
+  }
 }
